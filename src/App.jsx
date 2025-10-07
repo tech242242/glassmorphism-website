71ba4f02269d6d8fbee6f19aeaffb06335dbb126
muhammad saqib <tiktokusr4300@gmail.com>
2025-10-07 21:52:24 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import Features from './components/Features'
-import Footer from './components/Footer'
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-950 via-purple-900 to-indigo-950 text-white">
+    <div className="min-h-screen overflow-x-hidden">
       <Navbar />
       <HeroSection />
       <Features />
       <Footer />
     </div>
-  )
+  );
 }
-
-export default App
