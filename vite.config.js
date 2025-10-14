24972bcee85769ec18a80e90d2264ebdb731cdb9
muhammad saqib <tiktokusr4300@gmail.com>
2025-10-14 15:22:23 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-type-animation'], 
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src', 
+    },
+  },
 })
